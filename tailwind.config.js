cd527882b752995de08dd2b9680e88c04a638e4f
Prince Singh Chouhan <princesinghchouhan7470@gmail.com>
2024-04-07 14:40:18 +0530

    responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   content: ["./src/*.{js}", "./*.html"],
   theme: {
     screens: {
-      'tablet': {'max': '1024px'}
+      'tablet': {'max': '1024px'},
+      'phone': {'max': '500px'}
     },
     extend: {},
   },
